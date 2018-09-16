71fe4d74cd8fa9eb6e829ceb5a59c991e2e246ec
Ilya Kutko <36884482+DyatelVudu@users.noreply.github.com>
2018-09-16 18:46:28 +0300

    first implementation

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
+
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+	constructor() {
+		// your implementation
+		this.arr = [];
+		this.comparator = function(a, b){
+      return a - b;
+    }
+		}
 
-  add(element) {
-    // your implementation
-  }
+	add(element) {
+		// your implementation
+		this.arr.push(element);
+	}
 
-  at(index) {
-    // your implementation
-  }
+	at(index) {
+		// your implementation
+		return this.arr[index];
+	}
 
-  get length() {
-    // your implementation
-  }
+	get length() {
+		// your implementation
+		return this.arr.length;
+	}
 
-  toArray() {
-    // your implementation
-  }
+	toArray() {
+		// your implementation
+		return this.arr;
+	}
 
-  sort(indices) {
-    // your implementation
-  }
+	sort(indices) {
+		// your implementation
+		let array = [];
+		indices.sort((a, b) => a - b);
+		for(let i = 0; i < indices.length; i++){
+			array.push(this.arr[indices[i]]);
+		}
+		array.sort(this.comparator);
+			for(let i = 0; i < indices.length; i++){
+				this.arr[indices[i]] = array[i]; 
+			}
+		}
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+	setComparator(compareFunction) {
+		// your implementation
+		this.comparator = compareFunction;
+	}
 }
 
 module.exports = Sorter;
